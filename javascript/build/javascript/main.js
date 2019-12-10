35dcf5de6c988f100cebd7dd4ed6bfc04204452f
Nick Hemsley <nick.hems@gmail.com>
2019-12-11 10:44:45 +1100

    publish

--- javascript/build/javascript/main.js
+++ javascript/build/javascript/main.js
@@ -1406,22 +1406,3 @@ function cloud () {
 }
 
 cloud();
-
-/* swapping code */
-// var svg = skillset.getElementsByTagName('svg')[0]
-// svg.style.display = 'none';
-
-// var skillsetHtml = skillset.getElementsByClassName('skillset-body')[0]
-
-// var skillsetTitle = document.getElementsByClassName('skillset-title')[0]
-// console.log(svg, skillsetHtml, skillsetTitle)
-
-// skillsetTitle.addEventListener('mouseenter', function(event) {
-//   skillsetHtml.style.display = 'none'
-//   svg.style.display = 'block'
-// })
-
-// skillsetTitle.addEventListener('mouseleave', function(event) {
-//   skillsetHtml.style.display = 'flex'
-//   svg.style.display = 'none'
-// })
